41490bd2c3f11c138177490cdfc6c2a13d4f1a41
Ignacio <ignaciogarate@gmail.com>
2021-03-25 16:30:37 -0300

    full

---
+++ myApp.js
@@ -0,0 +1,41 @@
+var bGround = require('fcc-express-bground');
+var express = require('express');
+var app = express();
+bGround.log("Hello World");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ module.exports = app;
